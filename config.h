c0b55235b1973c1ef337c0c575ff816204b9d7c2
KoNicks <konicksdev@gmail.com>
2021-03-06 20:08:20 +0200

    increased gappx

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "togg
 /* See LICENSE file for copyright and license details. */
 /* appearance */
 static const unsigned int borderpx = 2;   /* border pixel of windows */
-static const unsigned int gappx     = 18;       /* gap pixel between windows */
+static const unsigned int gappx     = 20;       /* gap pixel between windows */
 static const unsigned int snap     = 32;  /* snap pixel */
 static const int showbar           = 1;   /* 0 means no bar */
 static const int topbar            = 1;   /* 0 means bottom bar */
