82c9d0ed18b122298638bff2842af226fe33c1cd
KoNicks <konicksdev@gmail.com>
2021-03-18 15:02:49 +0200

    tweaked bar again

--- dwm.c
+++ dwm.c
@@ -720,7 +720,7 @@ drawbar(Monitor *m)
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		if (occ & 1 << i)
-			drw_rect(drw, x + boxw + 1, 0, w - ( 2 * boxw + 1), boxw - 3,
+			drw_rect(drw, x /*+ boxw*/, 0, w /*- ( 2 * boxw + 1)*/+7, boxw - 3,
 			    m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
 			    urg & 1 << i);
 
